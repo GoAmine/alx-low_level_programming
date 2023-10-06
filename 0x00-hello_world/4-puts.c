c3da4f569be9e9b176e79edf1ccbd6bf1e279707
Amine <recruteamine12@gmail.com>
2023-10-05 20:05:52 -0700

    4-puts.C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (success)
+ * main - print "Programming is like building a multilingual
+ * puzzle, followed by a new line."
+ * return 0.
  */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	
+        return(0);
 }
