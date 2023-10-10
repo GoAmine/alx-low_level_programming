423c30a8941899e286e022c710a5a7a6a7491314
Amine <recruteamine12@gmail.com>
2023-10-09 18:09:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * This function prints a message to the console.
- *
- * Return: Always 0 (indicating success)
- */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
